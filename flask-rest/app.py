a2d06ab1ac6383632a0f5d4eb6c7104b021e5ba1
Abhishek Mondal <abhishek.mondal@live.in>
2021-01-23 16:48:35 +0530

    Added Logging capability

--- flask-rest/app.py
+++ flask-rest/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, json, Response
 from pymongo import MongoClient
-import logging as log
+import logging
+from logging.handlers import RotatingFileHandler
 
 
 app = Flask(__name__)
@@ -8,6 +9,8 @@ app = Flask(__name__)
 
 class MongoAPI:
     def __init__(self, data) :
+        app.logger.info('Instantiate DB Connection: ');
+ 
         self.client = MongoClient("mongodb://localhost:27017/")
 
         database = data ['database']
@@ -16,15 +19,19 @@ class MongoAPI:
         self.collection = cursor[collection]
         self.data = data
 
+        app.logger.info('Database: '+database);
+        app.logger.info('Database: '+collection);
+
         pass
     
     def read(self):
+        app.logger.info('Read All Data from Collection');
         documents = self.collection.find()
         output = [{item: data[item] for item in data if item != '_id'} for data in documents]
         return output
     
     def write(self, data):
-        log.info('Writing Data');
+        app.logger.info('Writing Data');
         new_document = data['Document']
         response = self.collection.insert_one(new_document);
         output = {'Status': 'Successfully Inserted',
@@ -32,6 +39,7 @@ class MongoAPI:
         return output
     
     def update(self, data):
+        app.logger.info('Updating Data');
         filt = data['Filter']
         updated_data = {"$set": data['DataToBeUpdated']}
         response = self.collection.update_one(filt, updated_data);
@@ -39,12 +47,15 @@ class MongoAPI:
         return output
     
     def delete(self,data):
+        app.logger.info('Deleting Data');
         filt = data['Filter']
         response = self.collection.delete_one(filt);
         output = {'Status': 'Successfuly Deleted' if response.deleted_count>0 else 'No Document Found for deletion'}
         return output
 
-
+def logResponse(response, operation):
+    app.logger.info('Response of operation: '+operation)
+    app.logger.info(json.dumps(response))
 
 @app.route('/')
 def base():
@@ -56,11 +67,13 @@ def base():
 def mongo_read():
     data = request.json
     if data is None or data == {}:
+        app.logger.info('Missing DB Conenction Informaion');
         return Response(response=json.dumps({"Error": "Please provide connection information"}),
                         status=400,
                         mimetype='application/json')
     obj1 = MongoAPI(data)
     response = obj1.read()
+    logResponse(response, 'Read');
     return Response(response=json.dumps(response),
                     status=200,
                     mimetype='application/json')
@@ -70,11 +83,13 @@ def mongo_read():
 def mongo_write():
     data = request.json
     if data is None or data == {} or 'Document' not in data:
+        app.logger.error('Missing DB Conenction Informaion');
         return Response(response=json.dumps({"Error": "Please provide connection information"}),
                         status=400,
                         mimetype='application/json')
     obj1 = MongoAPI(data)
     response = obj1.write(data)
+    logResponse(response, 'Insert');
     return Response(response=json.dumps(response),
                     status=200,
                     mimetype='application/json')
@@ -83,11 +98,13 @@ def mongo_write():
 def mongo_update():
     data = request.json
     if data is None or data == {} or 'Filter' not in data:
+        app.logger.error('Missing DB Conenction Informaion');
         return Response(response=json.dumps({"Error": "Please provide connection information"}),
                         status=400,
                         mimetype='application/json')
     obj1 = MongoAPI(data)
     response = obj1.update(data)
+    logResponse(response, 'Update');
     return Response(response=json.dumps(response),
                     status=200,
                     mimetype='application/json')
@@ -97,14 +114,26 @@ def mongo_update():
 def mongo_delete():
     data = request.json
     if data is None or data == {} or 'Filter' not in data:
+        app.logger.error('Missing DB Conenction Informaion');
         return Response(response=json.dumps({"Error": "Please provide connection information"}),
                         status=400,
                         mimetype='application/json')
     obj1 = MongoAPI(data)
     response = obj1.delete(data)
+    logResponse(response, 'Delete');
     return Response(response=json.dumps(response),
                     status=200,
                     mimetype='application/json')
   
 if __name__ == '__main__':
+    ##Add logger
+    handler = RotatingFileHandler('flask_rest.log', maxBytes=10000, backupCount=1)
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter(
+        "[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s")
+    handler.setFormatter(formatter)
+    app.logger.setLevel(logging.INFO)
+    app.logger.addHandler(handler)
+    
+
     app.run(debug=True, port=5001, host='0.0.0.0')
